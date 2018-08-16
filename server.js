2806b2143ed06b19c7041f7c0d9f8c0f37d97ed8
Jack Franklin <jack@jackfranklin.net>
2018-08-16 07:51:07 +0100

    cors

--- server.js
+++ server.js
@@ -5,13 +5,24 @@ const schema = require('./schema')
 
 app.use(
   '/graphql',
-  graphqlHTTP(req => {
-    return {
-      schema,
-      context: {},
-      graphiql: true
+  (req, res, next) => {
+    res.header('Access-Control-Allow-Credentials', true)
+    res.header(
+      'Access-Control-Allow-Headers',
+      'content-type, authorization, content-length, x-requested-with, accept, origin'
+    )
+    res.header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS')
+    res.header('Allow', 'POST, GET, OPTIONS')
+    res.header('Access-Control-Allow-Origin', '*')
+    if (req.method === 'OPTIONS') {
+      res.sendStatus(200)
+    } else {
+      next()
     }
+  },
+  graphqlHTTP({
+    schema,
+    graphiql: true,
   })
 )
-
 app.listen(4000)
